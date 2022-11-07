ce120abc10d7b4cd6cb83521b0adeffac3ba6285
KrishNaveenNK <krishnaveen021@gmail.com>
2022-11-07 21:47:37 +0530

    methods from tester2

--- src/main/java/org/nvn/Nvn.java
+++ src/main/java/org/nvn/Nvn.java
@@ -11,4 +11,16 @@ System.out.println("Greens");
 	public void method3() {
 System.out.println("Greens");
 	}
+	public void method11() {
+		System.out.println("Greens");
+
+	} 
+	public void method12() {
+		System.out.println("Greens");
+
+	}
+
+		public void method13() {
+	
+		System.out.println("Greens");}
 }
