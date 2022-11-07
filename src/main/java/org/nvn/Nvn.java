c408fcd0b8e986d96671d5764b4897b1733af068
KrishNaveenNK <krishnaveen021@gmail.com>
2022-11-07 21:21:48 +0530

    first push

---
+++ src/main/java/org/nvn/Nvn.java
@@ -0,0 +1,14 @@
+package org.nvn;
+
+public class Nvn {
+	public void method1() {
+System.out.println("Greens");
+	}
+	public void method2() {
+System.out.println("Greens");
+	}
+	
+	public void method3() {
+System.out.println("Greens");
+	}
+}
