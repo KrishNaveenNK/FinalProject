dc7ba15883dc923f1f7553ee4b7ae7555649e175
KrishNaveenNK <krishnaveen021@gmail.com>
2022-11-07 21:34:12 +0530

    3 methods added

--- src/main/java/org/nvn/Nvn.java
+++ src/main/java/org/nvn/Nvn.java
@@ -11,4 +11,13 @@ System.out.println("Greens");
 	public void method3() {
 System.out.println("Greens");
 	}
+	public void method4() {
+		System.out.println("Greens");
+	}
+	public void method5() {
+		System.out.println("Greens");
+	}
+	public void method6() {
+		System.out.println("Greens");
+	}
 }
